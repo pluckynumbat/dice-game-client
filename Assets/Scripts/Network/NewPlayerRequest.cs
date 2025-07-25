c7293d8a8291071d91e842dadffd2360d217bf2f
Neehar Patil <pluckynumbat@gmail.com>
2025-07-25 22:23:33 +0530

    add the function to send the post request to the server with the player id in the request body, and returning the deserialized player data if the request is successful

--- Assets/Scripts/Network/NewPlayerRequest.cs
+++ Assets/Scripts/Network/NewPlayerRequest.cs
@@ -13,6 +13,38 @@ namespace Network
         private const string ServerHost = "http://localhost";
         private const string ServerPort = "8080";
         private const string Endpoint = "/profile/new-player";
+    
+        public async Task<PlayerData> Send(string playerID, int timeout = 30)
+        {
+            PlayerData playerData;
+        
+            string uri = $"{ServerHost}:{ServerPort}{Endpoint}";
+
+            NewPlayerRequestBody requestBody = new NewPlayerRequestBody() {playerID = playerID};
+            string postData = JsonUtility.ToJson(requestBody);
+        
+            UnityWebRequest postRequest = UnityWebRequest.Post(uri,postData, "application/json");
+            postRequest.timeout = timeout;
+        
+            // TODO: add session id as a custom header here
+
+            await postRequest.SendWebRequest();
+        
+            switch (postRequest.result)
+            {
+                case UnityWebRequest.Result.Success:
+                    Debug.Log($"success, new player response: {postRequest.downloadHandler.text}");
+                    playerData = JsonUtility.FromJson<PlayerData>(postRequest.downloadHandler.text);
+                    break;
+
+                default:
+                    Debug.LogError($"failure, reason: {postRequest.error}");
+                    playerData = default(PlayerData);
+                    break;
+            }
+        
+            return playerData;
+        }
     }
     
     [Serializable]
