27bbaac8e2f9c020db83da37cfa6daa39f529b9f
Neehar Patil <pluckynumbat@gmail.com>
2025-07-25 10:45:07 +0530

    created a new state manager which will hold current state of the game and perform state transitions

---
+++ Assets/Scripts/Model/StateManager.cs
@@ -0,0 +1,9 @@
+namespace Model
+{
+    /// <summary>
+    /// This manager holds the current state of the game and performs state transitions
+    /// </summary>
+    public class StateManager
+    {
+    }
+}
