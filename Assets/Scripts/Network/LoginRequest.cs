2fc55295d060c2e122a8627fb8bd526c3b01ab2a
Neehar Patil <pluckynumbat@gmail.com>
2025-07-26 20:01:46 +0530

    added the Send function for the Login request which will create the POST unity web request and add the auth header to it, send new user status in the body, wait on the response and forward the results to the caller

--- Assets/Scripts/Network/LoginRequest.cs
+++ Assets/Scripts/Network/LoginRequest.cs
@@ -13,6 +13,40 @@ namespace Network
         private const string ServerHost = "http://localhost";
         private const string ServerPort = "8080";
         private const string Endpoint = "/auth/login";
+    
+        public async Task<AuthLoginData> Send(string authString, bool isNewUser, int timeout = 10)
+        {
+            AuthLoginData authLoginData;
+        
+            string uri = $"{ServerHost}:{ServerPort}{Endpoint}";
+            
+            LoginRequestBody requestBody = new LoginRequestBody() {isNewUser = isNewUser};
+            string postData = JsonUtility.ToJson(requestBody);
+            
+            UnityWebRequest postRequest = UnityWebRequest.Post(uri,postData, "application/json");
+            
+            postRequest.timeout = timeout;
+            postRequest.SetRequestHeader("Authorization", authString); // add the authorization header
+
+            await postRequest.SendWebRequest();
+        
+            switch (postRequest.result)
+            {
+                case UnityWebRequest.Result.Success:
+                    Debug.Log($"success! your player id is: {postRequest.downloadHandler.text}");
+                    authLoginData.loginResponse = JsonUtility.FromJson<LoginResponse>(postRequest.downloadHandler.text);
+                    authLoginData.sessionID = postRequest.GetResponseHeader("Session-Id"); // get the session id from the header
+                    Debug.Log($"session id: {authLoginData.sessionID}");
+                    break;
+
+                default:
+                    Debug.LogError($"failure, reason: {postRequest.error}");
+                    authLoginData = default(AuthLoginData);
+                    break;
+            }
+        
+            return authLoginData;
+        }
     }
     
     [Serializable]
