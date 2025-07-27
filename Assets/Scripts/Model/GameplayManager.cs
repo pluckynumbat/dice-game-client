5a0a5823b5eb5d23a8af33ed9f44dd92ee0bab67
Neehar Patil <pluckynumbat@gmail.com>
2025-07-27 22:05:23 +0530

    barebones gameplay manager which will be responsible for the data and functionality related to entering and playing the dice game levels

---
+++ Assets/Scripts/Model/GameplayManager.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Network;
+using UnityEngine;
+
+namespace Model
+{
+    /// <summary>
+    /// Gameplay manager will handle all data and functionality related to playing the dice game levels
+    /// </summary>
+    public class GameplayManager
+    {
+        
+    }
+}
