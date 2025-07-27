ae4541477c3776bf0d9ebdbf1779d5a665f70591
Neehar Patil <pluckynumbat@gmail.com>
2025-07-27 22:00:38 +0530

    create the enter level post request, send it to the server, wait for the response, and forward it to the caller

--- Assets/Scripts/Network/EnterLevelRequest.cs
+++ Assets/Scripts/Network/EnterLevelRequest.cs
@@ -13,6 +13,38 @@ namespace Network
         private const string ServerHost = "http://localhost";
         private const string ServerPort = "8080";
         private const string Endpoint = "/gameplay/entry";
+    
+        public async Task<EnterLevelResponse> Send(string sessionID, string playerID, int level, int timeout = 30)
+        {
+            EnterLevelResponse response;
+        
+            string uri = $"{ServerHost}:{ServerPort}{Endpoint}";
+
+            EnterLevelRequestBody requestBody = new EnterLevelRequestBody() {playerID = playerID, level = level};
+            string postData = JsonUtility.ToJson(requestBody);
+        
+            UnityWebRequest postRequest = UnityWebRequest.Post(uri,postData, "application/json");
+            postRequest.timeout = timeout;
+        
+            postRequest.SetRequestHeader("Session-Id", sessionID);
+
+            await postRequest.SendWebRequest();
+        
+            switch (postRequest.result)
+            {
+                case UnityWebRequest.Result.Success:
+                    Debug.Log($"success, enter level response: {postRequest.downloadHandler.text}");
+                    response = JsonUtility.FromJson<EnterLevelResponse>(postRequest.downloadHandler.text);
+                    break;
+
+                default:
+                    Debug.LogError($"failure, reason: {postRequest.error}");
+                    response = default(EnterLevelResponse);
+                    break;
+            }
+        
+            return response;
+        }
     }
     
     [Serializable]
