003d83190ba37e3e23ddb68f9589804ebe690e7d
Neehar Patil <pluckynumbat@gmail.com>
2025-07-28 15:47:20 +0530

    stats screen takes the player stats data from the player manager and forwards it to its stats presenter

--- Assets/Scripts/Presentation/Stats/Screens/StatsScreen.cs
+++ Assets/Scripts/Presentation/Stats/Screens/StatsScreen.cs
@@ -1,3 +1,5 @@
+using Model;
+using Presentation.Stats.Presenters;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +8,7 @@ namespace Presentation.Stats.Screens
     public class StatsScreen : MonoBehaviour
     {
         [SerializeField] private Button closeButton;
+        [SerializeField] private StatsPresenter statsPresenter;
         
         private PlayerManager myPlayerManager;
         private StateManager myStateManager;
@@ -19,11 +22,25 @@ namespace Presentation.Stats.Screens
         private void OnEnable()
         {
             closeButton.onClick.AddListener(OnCloseButtonClicked);
+            UpdateDisplay();
         }
 
         private void OnDisable()
         {
             closeButton.onClick.RemoveAllListeners();
+        }
+        
+        private void UpdateDisplay()
+        {
+            if (myPlayerManager?.Stats.levelStats != null)
+            {
+                statsPresenter?.Init(myPlayerManager.Stats.levelStats);
+            }
+            else
+            {
+                // TODO: display a "no stats yet" message?
+                Debug.Log("no stats to display yet");
+            }
         }
 
         private void OnCloseButtonClicked()
