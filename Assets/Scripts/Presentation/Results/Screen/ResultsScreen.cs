1bbacbd713de44b3087a97322fdfd9a9609baa00
Neehar Patil <pluckynumbat@gmail.com>
2025-07-28 18:29:35 +0530

    initialize the results screen by providing it the manager dependencies during startup

--- Assets/Scripts/Presentation/Results/Screen/ResultsScreen.cs
+++ Assets/Scripts/Presentation/Results/Screen/ResultsScreen.cs
@@ -1,3 +1,4 @@
+using Model;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,15 @@ namespace Presentation.Results.Screen
     public class ResultsScreen : MonoBehaviour
     {
         [SerializeField] private Button continueButton;
+        
+        private GameplayManager myGameplayManager;
+        private StateManager myStateManager;
+        
+        public void Initialize(GameplayManager gameplayManager, StateManager  stateManager)
+        {
+            myGameplayManager = gameplayManager;
+            myStateManager = stateManager;
+        }
         
         private void OnEnable()
         {
