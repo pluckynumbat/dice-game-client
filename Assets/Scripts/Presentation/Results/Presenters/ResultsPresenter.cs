29f32caa22a29a31b27ee44f873a69c95f3d7567
Neehar Patil <pluckynumbat@gmail.com>
2025-07-30 12:07:47 +0530

    results presenter now displays more dynamic elements: if the level was won, we show the energy reward, and is a new level was unlocked we show that as well

--- Assets/Scripts/Presentation/Results/Presenters/ResultsPresenter.cs
+++ Assets/Scripts/Presentation/Results/Presenters/ResultsPresenter.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 namespace Presentation.Results.Presenters
 {
     /// <summary>
-    /// This shows the message based on level win / loss
+    /// This shows the message(s) based on level win / loss like which new level was unlocked etc
     /// </summary>
     public class ResultsPresenter : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI labelText;
+        [SerializeField] private GameObject winOnlyArea;
+        
+        [SerializeField] private TextMeshProUGUI titleText;
+        [SerializeField] private TextMeshProUGUI energyRewardText;
+        [SerializeField] private TextMeshProUGUI levelUnlockedText;
 
-        public void Init(string value)
+        public void Init(bool won, int energyReward, bool unlockedNewLevel, int levelUnlocked)
         {
-            labelText.text = value;
+            winOnlyArea?.SetActive(won);
+            
+            titleText.text = won ? "Level Won!" : "Level Lost";
+            energyRewardText.text = won ? $"Energy Won: {energyReward}" : "";
+            levelUnlockedText.text = unlockedNewLevel ? $"New Level: {levelUnlocked}" : "";
         }
     }
 }
