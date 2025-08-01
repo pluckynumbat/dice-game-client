6c6840a5fff03154ce221e93f59883b168daaba1
Neehar Patil <pluckynumbat@gmail.com>
2025-08-01 13:15:55 +0530

    enter level request set up as a  POST  example of using the net request manager to send the request

--- Assets/Scripts/Network/EnterLevelRequest.cs
+++ Assets/Scripts/Network/EnterLevelRequest.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.Networking;
+using Model;
 
 namespace Network
 {
@@ -10,43 +9,20 @@ namespace Network
     /// </summary>
     public class EnterLevelRequest
     {
-        private const string ServerHost = "http://localhost";
-        private const string ServerPort = "8080";
         private const string Endpoint = "/gameplay/entry";
-    
-        public async Task<EnterLevelResponse> Send(string sessionID, string playerID, int level, int timeout = 30)
+
+        public async Task<EnterLevelResponse> Send(string playerID, int level, RequestParams extraParams)
         {
-            EnterLevelResponse response;
-        
-            string uri = $"{ServerHost}:{ServerPort}{Endpoint}";
+            EnterLevelResponse enterLevelResponse =
+                await GameRoot.Instance.NetRequestManager.SendPostRequest<EnterLevelResponse, EnterLevelRequestBody>(
+                    Endpoint,
+                    new EnterLevelRequestBody() { playerID = playerID, level = level },
+                    extraParams);
 
-            EnterLevelRequestBody requestBody = new EnterLevelRequestBody() {playerID = playerID, level = level};
-            string postData = JsonUtility.ToJson(requestBody);
-        
-            UnityWebRequest postRequest = UnityWebRequest.Post(uri,postData, "application/json");
-            postRequest.timeout = timeout;
-        
-            postRequest.SetRequestHeader("Session-Id", sessionID);
-
-            await postRequest.SendWebRequest();
-        
-            switch (postRequest.result)
-            {
-                case UnityWebRequest.Result.Success:
-                    Debug.Log($"success, enter level response: {postRequest.downloadHandler.text}");
-                    response = JsonUtility.FromJson<EnterLevelResponse>(postRequest.downloadHandler.text);
-                    break;
-
-                default:
-                    Debug.LogError($"failure, reason: {postRequest.error}");
-                    response = default(EnterLevelResponse);
-                    break;
-            }
-        
-            return response;
+            return enterLevelResponse;
         }
     }
-    
+
     [Serializable]
     public struct EnterLevelRequestBody
     {
