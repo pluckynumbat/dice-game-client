e2e95a4511a53245a09af8fe6b8b28390e5a5efc
Neehar Patil <pluckynumbat@gmail.com>
2025-08-02 20:21:19 +0530

    level result error handling: added error handling in case the level result request fails... the player can retry sending the request in this case, in the hopes that the service might have come up again

--- Assets/Scripts/Presentation/Gameplay/Screen/GameplayScreen.cs
+++ Assets/Scripts/Presentation/Gameplay/Screen/GameplayScreen.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Model;
 using Network;
 using Presentation.Gameplay.Presenters;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -11,10 +12,14 @@ namespace Presentation.Gameplay.Screen
 {
     public class GameplayScreen : MonoBehaviour
     {
+        private const string RollButtonTextNormal = "Roll!";
+        private const string RollButtonTextOnRequestFail = "Send Result";
+        
         [SerializeField] private Button rollButton;
         [SerializeField] private RollCounterPresenter rollCounterPresenter;
         [SerializeField] private TargetNumberPresenter targetNumberPresenter;
         [SerializeField] private DicePresenter dicePresenter;
+        [SerializeField] private TextMeshProUGUI buttonLabelText;
 
         [SerializeField] private int minimumLevelResultDelayMilliseconds;
         
@@ -22,6 +27,8 @@ namespace Presentation.Gameplay.Screen
         private StateManager myStateManager;
 
         private List<int> rolls; // to send to the server in the level result request
+
+        private bool levelResultRequestSent = false;
         
         public void Initialize(GameplayManager gameplayManager, StateManager stateManager)
         {
@@ -35,6 +42,7 @@ namespace Presentation.Gameplay.Screen
             
             rolls = new List<int>();
             rollButton.interactable = true;
+            levelResultRequestSent = false;
             UpdateDisplay(myGameplayManager.CurrentLevelConfig.totalRolls, myGameplayManager.CurrentLevelConfig.target);
         }
 
@@ -47,9 +55,23 @@ namespace Presentation.Gameplay.Screen
         {
             rollCounterPresenter.Init(maxRolls);
             targetNumberPresenter.Init(targetNumber);
+            buttonLabelText.text = RollButtonTextNormal;
         }
 
         private async void OnRollButtonClicked()
+        {
+            if (!levelResultRequestSent)
+            {
+                await PerformDiceRoll(); // normal flow
+            }
+            else
+            {
+                await ResendLevelResult(); // post request failure state
+            }
+        }
+
+        // Roll the die, and if the win condition or loss condition is met, send the level result request to the server
+        private async Task PerformDiceRoll()
         {
             rollCounterPresenter.ConsumeRoll();
             
@@ -64,12 +86,40 @@ namespace Presentation.Gameplay.Screen
 
             if (win || lose)
             {
-                Task<LevelResult> levelResultTask = myGameplayManager.RequestLevelResult(rolls.ToArray());
+                Task<LevelResultResponse> levelResultTask = myGameplayManager.RequestLevelResult(rolls.ToArray());
                 Task minDelayTask = Task.Delay(minimumLevelResultDelayMilliseconds);  // added to let the player view the dice result before changing game state
                 
                 await Task.WhenAll(levelResultTask, minDelayTask);
-                
-                Debug.Log("level " + (levelResultTask.Result.won ? "won" : "lost"));
+
+                // the request failed, we will go into an error state here...
+                // stay on the screen and let the player press the roll button after some time...
+                if (string.IsNullOrEmpty(levelResultTask.Result.playerData.playerID))
+                {
+                    levelResultRequestSent = true;
+                    rollButton.interactable = true;
+                    buttonLabelText.text = RollButtonTextOnRequestFail;
+                    return;
+                }
+
+                // the request succeeded, move on to the level end state and show the result
+                Debug.Log("level " + (levelResultTask.Result.levelResult.won ? "won" : "lost"));
+                myStateManager.ChangeGameState(StateManager.GameState.LevelEnd);
+            }
+            else
+            {
+                rollButton.interactable = true;
+            }
+        }
+        
+        // the level result request was sent once already, but it failed, so resend it
+        private async Task ResendLevelResult()
+        {
+            rollButton.interactable = false;
+            LevelResultResponse levelResultResponse = await myGameplayManager.RequestLevelResult(rolls.ToArray());
+            
+            if (!string.IsNullOrEmpty(levelResultResponse.playerData.playerID))
+            {
+                Debug.Log("level " + (levelResultResponse.levelResult.won ? "won" : "lost"));
                 myStateManager.ChangeGameState(StateManager.GameState.LevelEnd);
             }
             else
