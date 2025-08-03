4380f00a2b07b6adf8c4adc554fe88627cebcdea
Neehar Patil <pluckynumbat@gmail.com>
2025-08-03 17:22:46 +0530

    added a new file to store constants used throughout the client

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+/// <summary>
+/// Constants shared across the client
+/// </summary>
+public class Constants : MonoBehaviour
+{
+    public const string ServerHost = "http://localhost";
+    
+    public const string AuthServerPort = "10101";
+    public const string DataServerPort = "20202";
+    public const string ConfigServerPort = "30303";
+    public const string ProfileServerPort = "40404";
+    public const string StatsServerPort = "50505";
+    public const string GameplayServerPort = "60606";
+}
